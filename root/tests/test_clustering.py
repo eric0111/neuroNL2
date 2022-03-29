fcaf0a5b972142ae82bc57776467126b8be22eb2
eb <eric.boniardi@mail.polimi.it>
2022-03-29 23:27:07 +0200

    tests

---
+++ root/tests/test_clustering.py
@@ -0,0 +1,6 @@
+from root.clustering import clustering
+
+print("Test")
+TIME_SERIES_FOLDER = "./bin/output/test/time_series/"
+OUTPUT_FOLDER = "./bin/output/test/"
+clustering(TIME_SERIES_FOLDER, OUTPUT_FOLDER)
---
+++ root/tests/test_image_cleaner.py
@@ -0,0 +1,16 @@
+from root.images_cleaner import images_cleaner
+
+# FILES  =  "/Users/eb/Desktop/test_files/files/"
+# CONFOUNDS =  "/Users/eb/Desktop/test_files/confounds/"
+# CLEANED_FOLDER = "/Users/eb/Desktop/test_files/cleaned/"
+# cleaner(FILES, CONFOUNDS, CLEANED_FOLDER)
+
+FILES  =  "/Users/eb/Desktop/stopsignal/"
+CONFOUNDS =  "/Users/eb/Desktop/stopsignal_confounds/"
+OUTPUT_FOLDER = "/Users/eb/Desktop/stopsignal_cleaned/"
+images_cleaner(FILES, CONFOUNDS, OUTPUT_FOLDER)
+
+# FILES = "/Users/eb/Desktop/stopsignal/"
+# CONFOUNDS =  "/Users/eb/Desktop/stopsignal_confounds/"
+# task = "stopsignal"
+# main(task , FILES, CONFOUNDS)
